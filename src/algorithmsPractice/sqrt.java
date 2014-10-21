9852bf1cf08d578b7ee3f6f56f40b1b2cc482716
Wenbo Lin <lwbshr@gmail.com>
2014-10-20 20:43:16 -0700

    implementSqrt

--- src/algorithmsPractice/sqrt.java
+++ src/algorithmsPractice/sqrt.java
@@ -2,9 +2,31 @@ package algorithmsPractice;
 
 public class sqrt {
 
+	public int calSqrt(int n) {
+		int start = 0;
+		int end = n;
+		int mid = 0;
+		int result = -1;
+		while(Math.abs(mid * mid - n) > 0) {
+			mid = (start + end) / 2;
+			if(mid * mid == n) {
+				result = mid;
+				break;
+			}
+			if(mid * mid < n && (mid + 1) * (mid + 1) > n) {
+				result = mid;
+				break;
+			}
+			if(mid * mid < n) start = mid + 1;
+			else end = mid - 1;
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		sqrt test = new sqrt();
+		System.out.println(test.calSqrt(1000));
 	}
 
 }
