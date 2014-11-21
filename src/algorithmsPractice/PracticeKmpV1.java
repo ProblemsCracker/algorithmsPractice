857ad267eaa468d55ff6890fc044b1b1418ea1d2
Wenbo Lin <lwbshr@gmail.com>
2014-11-21 12:46:54 -0800

    yy
    yy

---
+++ src/algorithmsPractice/PracticeKmpV1.java
@@ -0,0 +1,10 @@
+package algorithmsPractice;
+
+public class PracticeKmpV1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/algorithmsPractice/PracticeMergeSortV1.java
@@ -0,0 +1,10 @@
+package algorithmsPractice;
+
+public class PracticeMergeSortV1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
