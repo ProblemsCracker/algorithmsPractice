095ef4b50170c176a0d5daef651f9a70fcd989cd
Wenbo Lin <lwbshr@gmail.com>
2014-11-21 22:06:42 -0800

    as
    as

--- src/algorithmsPractice/CasualPractice.java
+++ src/algorithmsPractice/CasualPractice.java
@@ -7,25 +7,30 @@ public class CasualPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 1;
-		//for(int i = 0; i < 64; i++)
-			//System.out.println((i & 0x1f));
+//		int a = 1;
+//		//for(int i = 0; i < 64; i++)
+//			//System.out.println((i & 0x1f));
+//		
+//		int[] test = {2,3,1};
+//		
+//		List<Integer> testList = new ArrayList<>();
+//		
+//		testList.add(1);
+//		testList.add(1);
+//		testList.add(1);
+//		testList.add(1);
+//		testList.add(1);
+//		
+//		//can't use changing size in for loop
+//		for(int i = 0; i < testList.size(); i++) { 
+//			testList.add(1);
+//			//System.out.println(testList.size());
+//		}
+//		
+		String s = "avs";
+		String p = "bvs";
 		
-		int[] test = {2,3,1};
-		
-		List<Integer> testList = new ArrayList<>();
-		
-		testList.add(1);
-		testList.add(1);
-		testList.add(1);
-		testList.add(1);
-		testList.add(1);
-		
-		//can't use changing size in for loop
-		for(int i = 0; i < testList.size(); i++) { 
-			testList.add(1);
-			System.out.println(testList.size());
-		}
+		System.out.println(s.compareTo(p));
 		
 	}
 
